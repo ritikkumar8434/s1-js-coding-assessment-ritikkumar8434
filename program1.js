f2b8f749582464075ee99fe3d6ac1947691727e2
ritikkumar8434 <161965586+ritikkumar8434@users.noreply.github.com>
2024-09-30 13:38:16 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -3,9 +3,27 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+    let stack = [];
     
+    const bracketMap = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+    
+    for (let char of s) {
+        if (char in bracketMap) {
+            let topElement = stack.length > 0 ? stack.pop() : '#';
+            
+            if (bracketMap[char] !== topElement) {
+                return false;
+            }
+        } else {
+            stack.push(char);
+        }
+    }
+    
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
-
-
